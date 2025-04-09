cb1dd3ed0764d4b171725486039d5882d5696023
homegi <homegi1109@mail.cmu.edu.tw>
2025-04-09 16:33:49 +0800

    ADD login, register

--- ML.NET/ML.NET/Data/AppDbContext.cs
+++ ML.NET/ML.NET/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ML.NET.Models;
 
 namespace ML.NET.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
