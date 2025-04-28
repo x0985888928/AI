c367fac9d649cb308a341be8fedef890467d14a7
homegi <homegi1109@mail.cmu.edu.tw>
2025-04-28 16:50:12 +0800

    ADD PhoneOptions

---
+++ ML.NET/ML.NET/Models/PhoneOptions.cs
@@ -0,0 +1,20 @@
+﻿// Models/PhoneOptions.cs
+namespace ML.NET.Models
+{
+    public class PhoneOptions
+    {
+        public List<Company> Companies { get; set; } = new();
+    }
+
+    public class Company
+    {
+        public string Name { get; set; } = default!;
+        public List<Brand> Brands { get; set; } = new();
+    }
+
+    public class Brand
+    {
+        public string Name { get; set; } = default!;
+        public List<string> Types { get; set; } = new();
+    }
+}
